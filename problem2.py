117e27ee442dd6dd144ace02d15aa3c19aaab413
Abdul Rahman <uniquelyislam@gmail.com>
2019-01-01 00:24:18 +0800

    answered problem 2 - need to check correct or wrong

---
+++ problem2.py
@@ -0,0 +1,20 @@
+# This is for problem 2 of ProjectEuler https://projecteuler.net/problem=2
+firstNumber = 0
+secondNumber = 1
+thirdNumber = 1
+
+addedNumber = 0
+
+i = 1
+
+while thirdNumber < 4000000 :
+	thirdNumber = firstNumber + secondNumber
+	firstNumber = secondNumber
+	secondNumber = thirdNumber
+
+	i += 1
+
+	if (thirdNumber%2==0):
+		addedNumber = addedNumber + thirdNumber 
+
+print (addedNumber)
