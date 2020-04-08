e219ff6ae09a8bb8be5552ebcdac27ed5107579b
Abdul Rahman <uniquelyislam@gmail.com>
2020-04-08 10:28:29 +0800

    add problem 3

---
+++ problem3.py
@@ -0,0 +1,20 @@
+import eulerlib
+
+def compute():
+	n = 600851475143
+	while True:
+		p = smallest_prime_factor(n)
+		if p < n:
+			n //= p
+		else:
+			return str(n)
+
+# Returns the smallest factor of n, which is in the range [2, n]. The result is always prime.
+def smallest_prime_factor(n):
+	assert n >= 2
+	for i in range(2, eulerlib.sqroot(n) + 1):
+		if n % i == 0:
+			return i
+	return n  # n itself is prime
+
+print(compute())
